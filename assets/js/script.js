e25e88a8a1403c3040585b8de3efcebaa604786c
carlaamt <103076049+carlaamt@users.noreply.github.com>
2023-05-18 15:31:04 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,34 @@ btnMobile.addEventListener('click', function(){
     navbar.classList.toggle('nav-collapse');
     btnMobile.classList.toggle('btn-transform');
 })
+
+const containerShort = document.querySelector('.short-link');
+const link = document.querySelector('#input-link');
+const result = document.querySelector('.result');
+
+containerShort.addEventListener('click', (e) => {
+  e.preventDefault();
+  const url = link.value;
+
+  shortenUrl(url);
+});
+
+async function shortenUrl(url) {
+    try{
+        const api = await fetch(`https://api.shrtco.de/v2/shorten?url=`+ url);
+        const data = await api.json();
+        const short = document.createElement('div');
+        short.classList.add('item');
+        short.innerHTML = `
+            <p> ${data.result.short_link}</p>
+            <button class="copy-btn">Copy</button>`;
+        result.prepend(short);
+        const copyBtn = result.querySelector('.copy-btn');
+        copyBtn.addEventListener('click', () => {
+        navigator.clipboard.writeText(copyBtn.previousElementSibling.textContent);
+        });
+        link.value = "";
+    } catch (err){
+        console.log(err);
+    }
+}
